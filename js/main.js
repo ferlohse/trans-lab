08bc4ced2791e2d046511da3fbe43570f172ccb7
fernanda vilches <fernanada.lohse@gmail.com>
2018-06-28 12:15:28 -0400

    añadi indexsaldo.html y preguntas frecuentes.html

--- js/main.js
+++ js/main.js
@@ -1,19 +1,51 @@
+window.dataBip = (NTarjeta) => {
+fetch(`http://www.psep.cl/api/Bip.php?&numberBip=${NTarjeta}`)
+  .then(response => response.json())
+  .then( data => {
+  console.log(data)
+  })};
+
+
+  let mail= document.getElementById("email_contacto");
+  const btniniciar = document.getElementById('iniciar');
+  const btnSaldo = document.getElementById('VerSaldo');
+  const btnCalcTarifa = document.getElementById('CalcularTarifa');
+  const btnPerfil = document.getElementById('Perfil');
+  const btnPreguntasF = document.getElementById('Preguntas');
+  const btn = document.getElementById("agregar");//agregar Ntarjetas
+
+
+  
+ 
+function Saldo(){
+  window.location="indexSaldo.html"
+}
+
+
+const btonSaldo= document.getElementById("Saldo");
+const vista=document.getElementById("info");
 
 
 
+btonSaldo.addEventListener("click", event=>{
+  const nTarjeta= document.getElementById("nro").value;
+  fetch(`http://www.psep.cl/api/Bip.php?&numberBip=${nTarjeta}`)
+  .then(response=>response.json())
+  .then(datos=>{
+    console.log(datos);
+    imprimirSaldo(datos)
+  })
+})
 
-
-const boton = document.getElementById("verSaldo");
-fetch('http://www.psep.cl/api/Bip.php?&numberBip={´$nTarjeta´}')
-     .then(response => response.json())
-     .then( data => {
-       console.log(data)
-      });
+const imprimirSaldo= (datosTarjeta)=>{
+  let saldoTarjeta= datosTarjeta["Saldo  tarjeta"];
+  vista.innerHTML= "Tu saldo es " + saldoTarjeta;
+}
 
 
 
 //indexp boton para agregar las targetas 
-const btn = document.getElementById("agregar");
+/*const btn = document.getElementById("agregar");
 
 boton.addEventListener("click", () => {  
     let comments = document.getElementById("comment").value;
@@ -30,6 +62,4 @@ boton.addEventListener("click", () => {
     
     newComments.appendChild(contenedorElemento);
     cont.appendChild(newComments);
-})
-  
-        
+*/
